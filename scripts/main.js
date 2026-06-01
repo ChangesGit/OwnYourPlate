7d748a82c8b46da6e49294f3235c746dfd5abbdf
ChangesGit <akrour.adel.pro@gmail.com>
2026-06-01 19:43:50 +0200

    FEAT : Added the dynamic search bar 01_06_2026

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
+let products = [];
+
+let alreadyAddedSearch = [];
+let alreadyAddedPending = [];
+const searchResultBox = document.getElementById('search-results');
+
+async function loadProducts() {
+  try {
+    const response = await fetch("./scripts/products.json");
+    const data = await response.json();
+
+    products = data;
+    console.log(products);
+  } catch (error) {
+    console.error("Impossible d'accéder à la base de donnée :", error);
+  }
+}
+
+loadProducts();
+
 const burgerMenu = document.querySelector('#burger-menu nav');
 const burgerMenuButton = document.getElementById('menu-toggle');
 
@@ -10,3 +30,59 @@ burgerMenuButton.addEventListener('click', () => {
     }
 
 })
+
+let imgURL = "./assets/images/skyr-1.png"
+let imgAlt = "image test"
+let articleName = "test";
+let articleKJ = "test";
+let articleKCal = "test"
+let articleProteins = "test";
+let articleCarbs = "test";
+
+// placeArticleSearchResult(imgURL, imgAlt, articleName, articleKJ, articleKCal, articleProteins, articleCarbs);
+placeArticlePending(imgURL, imgAlt, articleName, articleKJ, articleKCal, articleProteins, articleCarbs)
+
+
+const searchBar = document.getElementById('searchBar');
+
+//TODO : Ajouter une limite d'items ? Ajouter la possibilité d'écrire plusieurs mots ? Vérifier que le mot n'est pas déjà dans la barre
+
+let searchResults = [];
+
+searchBar.addEventListener('input', () => {
+    searchResultBox.innerHTML = "";
+    console.log(searchBar.value)
+
+    products.forEach(product => {
+        let matches = false;
+        for(let i = 0; i < product.keywords.length; i++) {
+            if (product.keywords[i].includes(searchBar.value) && !searchResults.includes(product)) {
+                matches = true;
+            }
+            // if(!product.keywords[i].includes(searchBar.value) && searchResults.some(e => e.name === product.name)) {
+            //     const index = searchResults.findIndex(e => e.name === product.name);
+            //     searchResults.splice(index,1);
+            // }
+        }
+        if(matches) {
+            searchResults.push(product);
+        }
+        else if(!matches && searchResults.some(e => e.name === product.name)) {
+            const index = searchResults.findIndex(e => e.name === product.name);
+            searchResults.splice(index, 1);
+        }
+        matches = false;
+    });
+
+    searchResults.forEach(product => {
+
+        if(!alreadyAddedSearch.includes(product.name)) {
+            alreadyAddedSearch.push(product.name);
+            placeArticleSearchResult(product.imgUrl, product.imgAlt, product.name, product.kJ, product.kCal, product.proteins, product.carbs)
+        }
+    })
+
+    searchResults = [];
+    alreadyAddedSearch = [];
+})
+
