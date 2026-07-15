3a235ec836084be7d60e6017505d43e8c66ed044
ChangesGit <akrour.adel.pro@gmail.com>
2026-07-15 16:50:35 +0200

    FIX : Fixed the SQL request, removed the first % so it's more efficient and optimised

--- search.php
+++ search.php
@@ -6,6 +6,6 @@
 
     //TODO : Ajouter la recherche sur les keywords en plus du nom
     $stmt = $mysqlClient->prepare('SELECT `name`, imgurl, kj, kcal, proteins, carbs, fat, saturated_fat, fibers, salt FROM products WHERE `name` LIKE ? OR keywords LIKE ? LIMIT 10');
-    $stmt->execute(["%".$userInput."%", "%".$userInput."%"]);
+    $stmt->execute([$userInput."%", $userInput."%"]);
 
     echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
