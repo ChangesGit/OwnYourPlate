bed5cf5fc21fe267b3f4317c5c120927b3b34a7e
ChangesGit <akrour.adel.pro@gmail.com>
2026-09-09 16:20:08 +0200

    FEAT : findbyEmail function

--- Model/ModelUser.php
+++ Model/ModelUser.php
@@ -10,7 +10,13 @@ class ModelUser extends Model {
     private ?int $type_id;
 
     public function findByEmail(string $email):array | bool {
-        $db = $this->model->getDb();
-        $db->prepare('SELECT u.user_id, u.')
+        try {
+            $db = $this->model->getDb();
+            $db->prepare('SELECT u.user_id, u.name, u.email, u.password, ut.type FROM users u INNER JOIN user_types ut ON u.type_id = ut.type_id WHERE u.email = ?');
+            $db->bindValue(1, $email, PDO::PARAM_STR);
+            return $db->execute();
+        } catch(ERROR $error) {
+            die($error);
+        }
     }
 }
